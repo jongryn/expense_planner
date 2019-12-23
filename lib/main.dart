2e4d56a13d5fdec2703a408e3dd8f4d98473e7c6
Jon Gryn <jongryn@Jons-iMac.local>
2019-12-23 11:35:35 -0800

    initial commit of expense planner files made with flutter/dart

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import './widgets/user_transactions.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PolarPro Filters App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  // String titleInput;
+  // String amountInput;
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PolarPro Filters App'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Color(0xffc99e1a),
+              child: Text('Shop'),
+              elevation: 5,
+            ),
+          ),
+          UserTransactions()
+        ],
+      ),
+    );
+  }
+}
